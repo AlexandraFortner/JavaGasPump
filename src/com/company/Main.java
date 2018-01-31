04a5248ff6c6928ed3e40a83d1996a9a0ba223d5
Alexandra <afortner@basecampcodingacademy.org>
2018-01-31 14:18:44 -0600

    Made temporary menu, and started a Receipt class.
    Next on my checklist is to round out the receipt class, and adding the price of the gas altogether.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,20 @@ public class Main {
         Scanner reader = new Scanner(System.in);
         System.out.println("What type of gas would you like?\n[R]egular [M]id-Grade [P]remium");
         String gasType = reader.nextLine();
+        if (gasType.equals("r")){
+            gasType = "Regular";
+            System.out.println("You've chosen Regular!");
+        }else if (gasType.equals("m")){
+            gasType = "Mid-Grade";
+            System.out.println("You've chosen Mid-Grade!");
+        }else if (gasType.equals("p")){
+            gasType = "Premium";
+            System.out.println("You've chosen Premium!");
+        } else {
+            System.out.println("Cannot compute...");
+        }
+        System.out.println("Would you like to:\n1. Pay Before\n2. Pay After");
+        String beforeOrAfter = reader.nextLine();
+        System.out.println("Thank you!");
     }
 }
