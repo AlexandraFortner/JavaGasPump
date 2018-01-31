ca4fb408d6cbda131f06a1ca1ddd26aafeabc664
Alexandra <afortner@basecampcodingacademy.org>
2018-01-31 13:06:01 -0600

    Created README.md from Github.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What type of gas would you like?\n[R]egular [M]id-Grade [P]remium");
+        String gasType = reader.nextLine();
     }
 }
